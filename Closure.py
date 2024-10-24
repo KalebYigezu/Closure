0b4b38e6b8b29acb81993982e2115d74817ac701
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-24 16:00:41 -0700

    Update Closure.py

--- Closure.py
+++ Closure.py
@@ -1,1 +1,16 @@
+def enter_number_outer():
+    numbers = []
 
+    def enter_number(x):
+        numbers.append(x)
+        print(numbers)
+
+    return enter_number
+
+
+enter_number = enter_number_outer()
+enter_number(5)
+enter_number(7)
+enter_number(9)
+
+https://www.youtube.com/watch?v=yiSdpYmZA2w
